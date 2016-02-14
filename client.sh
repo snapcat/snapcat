9628ea72e41f58d9379fcf3668a55c7dfdb9924c
Aaron DeVera <adevera@fordham.edu>
2016-02-13 20:08:27 -0500

    its working, but badd parsing for new lines

--- client.sh
+++ client.sh
@@ -1,9 +1,8 @@
-if [ -n "$2" ]; then
-    USER=id | cut -d ')' -f 1 - | cut -d '(' -f 2 -
-    TIME=`date | head --bytes 16 | tail --bytes 5`
-    echo "$TIME : $USER : $2" > msg
-    echo "" >> msg
-    curl -d "@./msg" localhost:$1
-else
-    curl localhost:$1
-fi
+while true
+do
+    clear
+    curl -s localhost:$1 > lc
+    cat lc | tr -d '\r\n' | sed -e '$a\'
+    sleep 2
+    
+done
--- server.sh
+++ server.sh
@@ -1,7 +1,9 @@
-DATE=`echo "SNAPCAT CHAT" > log; date>>log; echo "">>log;`
+#DATE=`echo "SNAPCAT CHAT" > log; date>>log; echo "">>log;`
 while true
 do 
   #cat some_file_to_server | nc -l 8008 | tail -n 1 >> log_file
-  nc -l $1 | tail -n 1 >> log
-  echo "" >> log
+  #nc -l $1 | tail -n 1 >> log
+  #cat log | 
+  nc -l $1 < log | tail -n 1 >> log
+  #cat log | sed '/^$/d' > log
 done
